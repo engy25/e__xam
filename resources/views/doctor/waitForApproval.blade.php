5e60790c0fbf11249fc58dad93ed56d0e492400a
3bdelmaksoud <abdelmaksoudelsayed1@gmail.com>
2022-05-10 02:34:37 +0200

    Doctor Section

---
+++ resources/views/doctor/waitForApproval.blade.php
@@ -0,0 +1,18 @@
+﻿@extends('layouts/doctor.app')
+@section('content')
+
+    <!--content start-->
+
+    <div class="content-page">
+        
+        <h1>Hello Teacher</h1>
+        <p class="p1">Your Account is not approved till now <br><br>Our Team is checking your profile <br><br> Soon Your Account Will Be Approved</p>
+        <hr>
+        <p class="p2">Check Later</p>   
+        <a href="{{route('auth.login')}}">Logout For Now</a>
+         
+    </div>
+
+    <!--content end-->
+    @endsection
+</html>
